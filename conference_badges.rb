3702d86a5e16afa8ae520968ff47ed7c022f989b
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-05 14:05:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+conference_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+badge_maker(array)
+  array.each do |speakers|
+  "Hello, my name is #{conference_speakers}."
+end 
+# batch_badge_creator(array)
+
+assign_rooms 
+
+printer 
+
